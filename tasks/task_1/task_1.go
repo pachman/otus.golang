2e74735ec3e7475162c5c38a334924659d26f595
pachman <pachman@bk.ru>
2019-12-01 18:58:03 +0300

    init task 1

---
+++ tasks/task_1/task_1.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"os"
+)
+
+type Time struct {
+	Hour    int
+	Minute  int
+	Seconds int
+}
+
+func (ext Time) String() string {
+	return fmt.Sprintf("%02d:%02d:%02d", ext.Hour, ext.Minute, ext.Seconds)
+}
+
+func main() {
+	now, err := timeNow()
+
+	if err == nil {
+		fmt.Println("Time now:", now)
+	} else {
+		fmt.Fprintf(os.Stderr, "Error: %s", err)
+		os.Exit(1)
+	}
+}
+
+func timeNow() (Time, error) {
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+
+	if err == nil {
+		return Time{
+			Hour:    time.Hour(),
+			Minute:  time.Minute(),
+			Seconds: time.Second(),
+		}, nil
+	} else {
+		return Time{}, err
+	}
+}
